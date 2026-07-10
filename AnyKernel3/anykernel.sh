f879f7b20d78c8154ac0fedcaf877392c106c745
XuexGao <XuexGao@users.noreply.github.com>
2026-07-10 02:32:44 +0000

    feat: 添加 DroidSpaces 容器支持 + 修复 selinux_hide.c 链接错误
    DroidSpaces 支持:
    - 开启 CONFIG_PID_NS=y (之前被显式禁用)
    - 开启 CONFIG_UTS_NS/y, IPC_NS/y, NET_NS/y, USER_NS/y
    - 开启 CONFIG_FHANDLE=y (systemd 需要)
    - 开启 CONFIG_SYSVIPC=y + CONFIG_POSIX_MQUEUE=y (IPC_NS 依赖)
    - 开启 CONFIG_SECCOMP_FILTER=y
    - 开启 CONFIG_VETH=y + CONFIG_MACVLAN=y (容器网络)
    - 开启 CONFIG_CGROUP_DEVICE/y, CGROUP_PIDS/y, CGROUP_NET_PRIO/y
    
    selinux_hide.c 修复 (KernelSU-Next 上游 bug):
    - #if defined(CONFIG_KPROBES) → #ifdef KSU_KPROBES_HOOK
      (extras.c 中 kprobe 符号用 KSU_KPROBES_HOOK 守卫, manual hook
       模式下不编译, 但 selinux_hide.c 用 CONFIG_KPROBES 引用导致
       链接错误: undefined reference to slow_avc_audit_kp)
    - selinux_state extern 添加 KSU_COMPAT_USE_SELINUX_STATE 守卫
      (4.9 内核无 struct selinux_state)
    
    AnyKernel3: 从 submodule 改为直接包含, 添加 R9s 配置
    - flash_boot (跳过 ramdisk 重组)
    - NO_MAGISK_CHECK=1
    - sysfs 分区大小检查 (/sys/class/block/*/size)

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,45 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=KernelSU-Next v3.2.0 for OPPO R9s (LineageOS 21, kernel 4.9.337) + DroidSpaces
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=r9s
+device.name2=R9s
+device.name3=16017
+device.name4=16027
+device.name5=msm8953
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=auto;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+NO_MAGISK_CHECK=1;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+split_boot;
+
+flash_boot;
+## end boot install
